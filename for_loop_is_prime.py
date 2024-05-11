36b4323bf425738b9f57b207e3ae1201859f97bf
Vusale Soylemezo <vusalesoylemezo@Vusales-MacBook-Air.local>
2024-05-10 20:32:16 -0400

    created for_loop_is_prime.py

---
+++ for_loop_is_prime.py
@@ -0,0 +1,12 @@
+start = int(input("Enter the start of the range: "))
+end = int(input("Enter the end of the range: "))
+
+for num in range(start, end + 1):
+    if num > 1:
+        is_prime = True
+        for i in range(2, int(num ** 0.5) + 1):
+            if num % i == 0:
+                is_prime = False
+                break
+        if is_prime:
+            print(num)
